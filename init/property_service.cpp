ce33e33f0f509b161d6a04e718f0c342f30d09dd
Alex Naidis <alex.naidis@linux.com>
2017-04-09 01:29:27 +0200

    [sys/core] init: Weaken property override security for the init extension
    Sometimes we need to override ro.* properties by using
    our vendor init extension.
    
    Previously there was a security check which was blocking
    that.
    To resolve the issue, we need to weaken the security check
    during the execution of our vendor init extension.
    
    This is safe because the vendor init extension gets
    executed as part of init construction and it is
    considered a trusted system component.
    
    Change-Id: Id91c4ac1f60217dc83dd1cedad9880c9f0fc5f91
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -69,6 +69,8 @@ static int persistent_properties_loaded = 0;
 
 static int property_set_fd = -1;
 
+static bool weaken_prop_override_security = false;
+
 void property_init() {
     if (__system_property_area_init()) {
         LOG(ERROR) << "Failed to initialize property area";
@@ -184,7 +186,7 @@ static uint32_t PropertySetImpl(const std::string& name, const std::string& valu
     prop_info* pi = (prop_info*) __system_property_find(name.c_str());
     if (pi != nullptr) {
         // ro.* properties are actually "write-once".
-        if (android::base::StartsWith(name, "ro.")) {
+        if (android::base::StartsWith(name, "ro.") && !weaken_prop_override_security) {
             LOG(ERROR) << "property_set(\"" << name << "\", \"" << value << "\") failed: "
                        << "property already set";
             return PROP_ERROR_READ_ONLY_PROPERTY;
@@ -752,6 +754,12 @@ void load_system_props() {
 
     // Update with vendor-specific property runtime overrides
     vendor_load_properties();
+
+    /* Weaken property override security during execution of the vendor init extension. */
+    weaken_prop_override_security = true;
+
+    /* Restore the normal property override security after vendor init extension is executed. */
+    weaken_prop_override_security = false;
 
     load_recovery_id_prop();
 }
